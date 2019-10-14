2f2a5dea7de73d5163a565dd72d0fa151adb5eb9
Anita Rawat Patankar <anita.rawat-patankar@capgemini.com>
2019-10-14 17:48:57 +0530

    Updated premium post api

--- smart_insurance/premium.py
+++ smart_insurance/premium.py
@@ -19,10 +19,10 @@ def get_premium():
         basic_value = data.get("basic_value", 0)
         miles_run = data.get("miles_run", 0)
         garage_condition = data.get("garage_condition", "Bad")
-        has_anti_theft = data.get("has_anti_theft", "false")
-        has_multi_policy = data.get("has_multi_policy", "false")
-        has_multi_car = data.get("has_multi_car", "false")
-        has_driver_training = data.get("has_driver_training", "false")
+        has_anti_theft = data.get("has_anti_theft", False)
+        has_multi_policy = data.get("has_multi_policy", False)
+        has_multi_car = data.get("has_multi_car", False)
+        has_driver_training = data.get("has_driver_training", False)
         accident_violation = data.get("accident_violation", 0)
         vehicle_number = data.get("vehicle_number", "Unknown")
         vin_number = data.get("vin_number", "Unknown")
@@ -59,25 +59,33 @@ def get_premium():
 
         premium = base_premium - base_premium * discount / 100
 
+        vehicle_info = {"detail": {
+                "vehicle_type": vehicle_type,
+                "basic_value": basic_value,
+                "miles_run": miles_run,
+                "garage_condition": garage_condition,
+                "has_anti_theft": has_anti_theft,
+                "has_multi_policy": has_multi_policy,
+                "has_multi_car": has_multi_car,
+                "has_driver_training": has_driver_training,
+                "accident_violation": accident_violation,
+                "vehicle_number": vehicle_number,
+                "vin_number": vin_number,
+                "vehicle_owner": vehicle_owner,
+                "vehicle_image_path": vehicle_image_path
+                },
+                "calculated": {
+                    "discount": discount,
+                    "premium": premium
+                }
+            }
+
         resp_dict = {"result":
-                         {"vehicle_type": vehicle_type,
-                          "basic_value": basic_value,
-                          "miles_run": miles_run,
-                          "garage_condition": garage_condition,
-                          "has_anti_theft": has_anti_theft,
-                          "has_multi_policy": has_multi_policy,
-                          "has_multi_car": has_multi_car,
-                          "has_driver_training": has_driver_training,
-                          "accident_violation": accident_violation,
-                          "vehicle_number": vehicle_number,
-                          "vin_number": vin_number,
-                          "vehicle_owner": vehicle_owner,
-                          "vehicle_image_path": vehicle_image_path,
-                          "premium": premium},
+                         {"vehicle_info": vehicle_info},
                      "response": "200"}
 
         # inserting data in dynamo db
-        __insert_vehicle_info_into_dynamo(resp_dict)
+        __insert_vehicle_info_into_dynamo(vehicle_info)
 
         response = Response(json.dumps(resp_dict), 200)
 
@@ -95,21 +103,24 @@ def get_premium():
 def __insert_vehicle_info_into_dynamo(vehicle_info):
     try:
         dynamodb = boto3.client('dynamodb')
+        vehicle_details = vehicle_info['detail']
+        vehicle_calculated = vehicle_info['calculated']
         item = {
-            'vehicle_type': {'S': vehicle_info['vehicle_type']},
-            'basic_value': {'S': str({'basic_value': vehicle_info['basic_value']})},
-            'miles_run': {'S': str({'miles_run': vehicle_info['miles_run']})},
-            'garage_condition': {'S': vehicle_info['garage_condition']},
-            'has_anti_theft': {'B': vehicle_info['has_anti_theft']},
-            'has_multi_policy': {'B': vehicle_info['has_multi_policy']},
-            'has_multi_car': {'B': vehicle_info['has_multi_car']},
-            'has_driver_training': {'B': vehicle_info['has_driver_training']},
-            'accident_violation': {'S': str({'accident_violation': vehicle_info['accident_violation']})},
-            'vehicle_number': {'S': vehicle_info['vehicle_number']},
-            'vin_number': {'S': vehicle_info['vin_number']},
-            'vehicle_owner': {'S': vehicle_info['vehicle_owner']},
-            'vehicle_image_path': {'S': vehicle_info['vehicle_image_path']},
-            'premium': {'S': str({'basic_value': vehicle_info['premium']})},
+            'vehicle_type': {'S': vehicle_details['vehicle_type']},
+            'basic_value': {'S': str({'basic_value': vehicle_details['basic_value']})},
+            'miles_run': {'S': str({'miles_run': vehicle_details['miles_run']})},
+            'garage_condition': {'S': vehicle_details['garage_condition']},
+            'has_anti_theft': {'B': vehicle_details['has_anti_theft']},
+            'has_multi_policy': {'B': vehicle_details['has_multi_policy']},
+            'has_multi_car': {'B': vehicle_details['has_multi_car']},
+            'has_driver_training': {'B': vehicle_details['has_driver_training']},
+            'accident_violation': {'S': str({'accident_violation': vehicle_details['accident_violation']})},
+            'vehicle_number': {'S': vehicle_details['vehicle_number']},
+            'vin_number': {'S': vehicle_details['vin_number']},
+            'vehicle_owner': {'S': vehicle_details['vehicle_owner']},
+            'vehicle_image_path': {'S': vehicle_details['vehicle_image_path']},
+            'discount': {'S': str({'discount': vehicle_calculated['discount']})},
+            'premium': {'S': str({'premium': vehicle_calculated['premium']})}
         }
         dynamodb.put_item(TableName='vehicle-premium-info', Item=item)
     except Exception as e:
